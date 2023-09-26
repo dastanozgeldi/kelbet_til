3de71ef710b0a6d1d238b01d18d53ab4e9ce8969
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-09-26 21:49:02 +0600

    feat: add all literary works

--- src/components/grade-selector.tsx
+++ src/components/grade-selector.tsx
@@ -1,14 +1,20 @@
+"use client";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
 
 export const GradeSelector = ({ data }: { data: any }) => {
   const grades = ["7", "8", "9", "10", "11", "12"];
   const languages = ["Қазақша", "Орысша"];
+  const [grade, setGrade] = useState(grades[0]);
+  const [language, setLanguage] = useState(languages[0]);
+
+  console.log(grade, language)
 
   return (
     <div className="flex flex-col sm:flex-row sm:items-center gap-6">
       <div>
         <h2>Сынып</h2>
-        <Tabs defaultValue={grades[0]}>
+        <Tabs value={grade} onValueChange={setGrade}>
           <TabsList>
             {grades.map((grade) => (
               <TabsTrigger key={grade} value={grade}>
@@ -26,7 +32,7 @@ export const GradeSelector = ({ data }: { data: any }) => {
 
       <div>
         <h2>Оқыту тілі</h2>
-        <Tabs defaultValue={languages[0]}>
+        <Tabs value={language} onValueChange={setLanguage}>
           <TabsList>
             {languages.map((language) => (
               <TabsTrigger key={language} value={language}>
