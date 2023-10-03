280dd8f32ba26997fa4158f8cc3235dd4aec77d1
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-10-03 14:40:15 +0600

    feat: react pdf integration

--- src/app/[work]/page.tsx
+++ src/app/[work]/page.tsx
@@ -1,4 +1,12 @@
-import data from "../../../public/works.json";
+"use client";
+import { PDFBook } from "@/components/pdf-book";
+import { pdfjs } from "react-pdf";
+// import data from "../../../public/works.json";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
 
 export default function Page({ params }: { params: { work: string } }) {
   const work = decodeURIComponent(params.work);
@@ -11,27 +19,22 @@ export default function Page({ params }: { params: { work: string } }) {
         <h1 className="text-3xl my-2 md:text-4xl font-bold">{name}</h1>
         <hr className="border-0 max-w-[36px] h-[6px] bg-[#6C63FF]" />
       </div>
-      <embed
-        className="border"
-        src={`/works/${folder}/${name}.pdf`}
-        width="100%"
-        height="600px"
-      />
+      <PDFBook file={`/works/${folder}/${name}.pdf`} />
     </div>
   );
 }
 
-export async function generateStaticParams() {
-  const result = [];
+// export async function generateStaticParams() {
+//   const result = [];
 
-  for (const key in data) {
-    if (data.hasOwnProperty(key)) {
-      const namesArray = data[key as keyof typeof data].map((obj) => ({
-        name: `${obj.name}`,
-      }));
-      result.push(...namesArray);
-    }
-  }
+//   for (const key in data) {
+//     if (data.hasOwnProperty(key)) {
+//       const namesArray = data[key as keyof typeof data].map((obj) => ({
+//         name: `${obj.name}`,
+//       }));
+//       result.push(...namesArray);
+//     }
+//   }
 
-  return result;
-}
+//   return result;
+// }
