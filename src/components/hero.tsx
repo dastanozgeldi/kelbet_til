1832f933b922df391c70a78c8d5caf13f09a860b
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-11-13 11:40:56 +0600

    fix: remove dot

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,7 +8,7 @@ export const Hero = () => {
       <div className="w-full flex flex-col items-center lg:flex-row lg:justify-between lg:gap-8">
         <div className="lg:w-1/2 space-y-8">
           <h1 className="text-3xl lg:text-4xl font-bold">
-            НЗМ әдеби шығармалары бір жерде.
+            НЗМ әдеби шығармалары бір жерде
           </h1>
 
           <div>
