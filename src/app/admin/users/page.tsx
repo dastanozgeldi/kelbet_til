555bc1e9a2f9b55e5b5874e5c49c43063ca86756
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-02-01 13:12:29 +0600

    feat: example users table

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <div className="my-6">other type shi</div>;
+import { User, columns } from "./columns";
+import { data } from "./data";
+import { DataTable } from "./data-table";
+
+async function getData(): Promise<User[]> {
+  // Fetch data from your API here.
+  return data;
+}
+
+export default async function DemoPage() {
+  const data = await getData();
+
+  return <DataTable columns={columns} data={data} />;
 }
--- src/components/admin/sidebar.tsx
+++ src/components/admin/sidebar.tsx
@@ -11,7 +11,7 @@ export const Sidebar = () => {
     <div className="fixed h-screen w-64 p-6 border-r">
       {/* sidebar header */}
       <div>
-        <h1 className="text-2xl font-bold">Администратор</h1>
+        <h1 className="text-xl font-bold">Администратор</h1>
       </div>
 
       <div className="my-5 w-full h-[1.5px] bg-gray-100" />
