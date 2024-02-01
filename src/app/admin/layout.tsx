2e6e477ac88ea9dea4e6f6f73b3e04f34485c61c
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-02-02 01:52:56 +0600

    fix: admin role check in whole admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,11 +1,18 @@
 import { Header } from "@/components/admin/header";
 import { Sidebar } from "@/components/admin/sidebar";
+import { NoAccess } from "@/components/errors";
+import { getServerAuthSession } from "@/server/auth";
 
-export default function AdminLayout({
+export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerAuthSession();
+
+  if (session?.user.role !== "ADMIN") {
+    return <NoAccess />;
+  }
   return (
     <div className="w-full grid grid-cols-[280px,1fr]">
       <div>
