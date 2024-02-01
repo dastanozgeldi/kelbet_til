20e4efa7c0c817bb186436d4a25b1a7f33f8bce3
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-02-02 01:52:56 +0600

    fix: admin role check in whole admin layout

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,15 +1,12 @@
 import { db } from "@/server/db";
-import { getServerAuthSession } from "@/server/auth";
-import { NoAccess } from "@/components/errors";
 import { Works } from "@/components/admin/works";
 
 export default async function Page() {
-  const session = await getServerAuthSession();
+  const books = await db.book.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
-  const books = await db.book.findMany({ orderBy: { createdAt: "desc" } });
-
-  if (session?.user.role !== "ADMIN") {
-    return <NoAccess />;
-  }
   return <Works books={books} />;
 }
