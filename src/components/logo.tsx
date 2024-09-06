43b4f45c2dc961e9c613e5a496f8d0013defcfd1
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-09-07 00:34:13 +0500

    feat: update logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <Link href="/">
-      <Image src="/logo.png" width={160} height={38} alt="Logo" />
+    <Link href="/" className="flex items-center space-x-2.5">
+      <Image src="/logo.png" width={32} height={32} alt="Logo" />
+      <div className="font-bold text-xl">kelbet-til.kz</div>
     </Link>
   );
 };
