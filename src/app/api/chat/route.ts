28daf8c37b3a8497d803477677761523e726cc6e
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-09-09 21:25:43 +0500

    feat: add a system prompt to chat route

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -5,11 +5,24 @@ import { streamText, convertToCoreMessages } from "ai";
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  const { bookTitle, messages } = await req.json();
+
+  const prompt = {
+    role: "system",
+    content:
+      `Сен -- қазақ әдебиетін өте жақсы білетін жасанды интеллектсің.\n` +
+      `Қазір сен Kelbet Til сайтында жұмыс істеп отырсың.\n` +
+      `Қолданушылар саған әдеби шығармалар бойынша сұрақ қояды.\n` +
+      `Сен қолданушымен тек бір тақырыпта сөйлесесің.\n` +
+      `Мысалы, сен "Ақбілек" романына енгізілген жасанды интеллектсің.\n` +
+      `Тек "Ақбілек" романы бойынша сұраққа жауап бересің.\n` +
+      `Тақырыптан тыс сұрақтарға "Мен әдеби чатботпын, әдебиеттен тыс сұрақтарға жауап бере алмаймын" деп жауап бересің` +
+      `Шығарманың тақырыбы: ${bookTitle}`,
+  };
 
   const result = await streamText({
     model: openai("gpt-4o"),
-    messages: convertToCoreMessages(messages),
+    messages: convertToCoreMessages([prompt, ...messages]),
   });
 
   return result.toDataStreamResponse();
