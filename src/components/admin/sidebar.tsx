64b18106315d01131ed3cc2b3fc67bd2b219ab6f
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-09-21 15:55:26 +0500

    feat: improve admin sidebar behavior for mobile

--- src/components/admin/sidebar.tsx
+++ src/components/admin/sidebar.tsx
@@ -12,33 +12,29 @@ export const Sidebar = () => {
   const pathname = usePathname();
 
   return (
-    <div className="fixed h-[90vh] w-64 py-8 pr-6 border-r">
-      <h1 className="text-xl font-bold">Администратор</h1>
-
-      <div className="my-5 w-full h-[1.5px] bg-gray-100" />
-
-      {/* sidebar links */}
+    <div className="fixed h-[87vh] w-12 border-r md:w-64">
       <div className="pt-4">
         {links.map(({ href, label, icon }) => (
           <Link
             key={label}
             href={href}
             className={cn(
-              "flex items-center gap-3 mb-6 text-sm",
-              href === pathname && "text-[#6C63FF]"
+              "mb-6 flex items-center gap-3 text-sm",
+              href === pathname && "text-[#6C63FF]",
             )}
           >
-            {icon} {label}
+            {icon}
+            <span className="hidden md:block">{label}</span>
           </Link>
         ))}
       </div>
 
-      {/* logout */}
       <button
-        className="absolute bottom-0 text-red-500 flex items-center gap-3 text-sm my-6"
+        className="absolute bottom-0 my-6 flex items-center gap-3 text-sm text-red-500"
         onClick={() => signOut()}
       >
-        <Icons.logout width={20} height={20} /> Шығу
+        <Icons.logout width={20} height={20} />
+        <span className="hidden md:block">Шығу</span>
       </button>
     </div>
   );
