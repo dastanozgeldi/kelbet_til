2a4ca57cb756c681745fb2dfd61a5c9d4b9f0d42
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-10-08 14:25:31 +0500

    feat: add link to form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { Nav } from "@/components/nav";
 import { Footer } from "@/components/footer";
 import { Toaster } from "@/components/ui/sonner";
+import { buttonVariants } from "@/components/ui/button";
 import { EdgeStoreProvider } from "@/lib/edgestore";
 import { cn } from "@/lib/utils";
 
@@ -32,6 +33,21 @@ export default function RootLayout({
       <body className={cn(inter.className, "min-h-screen px-6 py-4 lg:px-24")}>
         <EdgeStoreProvider>
           <main>
+            <div className="-mt-2 mb-4 flex w-full flex-col items-center justify-between sm:flex-row">
+              платформа бойынша пікір қалдырыңыз!
+              <a
+                href="https://forms.gle/RyySDQM5GwqXoLUV7"
+                target="_blank"
+                rel="noreferrer"
+                className={cn(
+                  buttonVariants({ variant: "outline" }),
+                  "w-full sm:w-auto",
+                )}
+              >
+                сауалнаманы өту
+              </a>
+            </div>
+
             <Nav />
             {children}
             <Footer />
