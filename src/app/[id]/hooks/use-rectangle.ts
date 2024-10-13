a5eb48f35536bc2b363ba4c09b995f88269bee45
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-10-13 13:18:08 +0500

    feat: isolate pdf book related hooks

--- src/app/[id]/hooks/use-rectangle.ts
+++ src/app/[id]/hooks/use-rectangle.ts
@@ -1,7 +1,6 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import { PDFDocumentProxy, PDFPageProxy } from "pdfjs-dist";
 import { isValidRectangle } from "@/lib/utils";
-import { toast } from "sonner";
 
 interface Rectangle {
   x: number;
@@ -10,67 +9,7 @@ interface Rectangle {
   height: number;
 }
 
-export const usePDFBook = (bookId: string) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [numPages, setNumPages] = useState(1);
-  const [isEditing, setIsEditing] = useState(false);
-
-  const hasMultiplePages = numPages >= 3;
-
-  const noPrevPage = !hasMultiplePages || currentPage === 1;
-  const noNextPage =
-    !hasMultiplePages ||
-    currentPage === numPages ||
-    currentPage + 1 === numPages;
-
-  const handleDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
-    setNumPages(numPages);
-  };
-
-  const handleNextPage = () => {
-    if (currentPage < numPages) {
-      const newPage = currentPage + 2;
-      setCurrentPage(newPage);
-      resetRectangle();
-    }
-  };
-
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      const newPage = currentPage - 2;
-      setCurrentPage(newPage);
-      resetRectangle();
-    }
-  };
-
-  useEffect(() => {
-    const getLastPage = () => {
-      const id = localStorage.getItem("last-book-id");
-      const page = Number(localStorage.getItem("last-book-page"));
-
-      if (id && id === bookId && page) {
-        setCurrentPage(page);
-      }
-    };
-
-    getLastPage();
-  }, [bookId]);
-
-  useEffect(() => {
-    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
-      localStorage.setItem("last-book-page", currentPage.toString());
-      localStorage.setItem("last-book-id", bookId);
-    };
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, [bookId, currentPage]);
-
-  // For translation
-
+export const useRectangle = () => {
   const [rectangle, setRectangle] = useState<Rectangle | null>(null);
   const [isDrawing, setIsDrawing] = useState(false);
   const [startPoint, setStartPoint] = useState<{ x: number; y: number } | null>(
@@ -214,17 +153,6 @@ export const usePDFBook = (bookId: string) => {
   );
 
   return {
-    numPages,
-    currentPage,
-    noPrevPage,
-    noNextPage,
-    isEditing,
-    setCurrentPage,
-    setIsEditing,
-    handlePrevPage,
-    handleNextPage,
-    handleDocumentLoadSuccess,
-    // For translation
     croppedImage,
     rectangle,
     pdfDocumentRef,
@@ -233,5 +161,6 @@ export const usePDFBook = (bookId: string) => {
     handleMouseDown,
     handleMouseMove,
     handleMouseUp,
+    resetRectangle,
   };
 };
