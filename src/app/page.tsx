629cab238db4f2fff1c2ccacced0a74e69cb3a7b
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-10-24 09:20:15 +0500

    feat: only show active books to general public

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import { db } from "@/server/db";
 
 export default async function Home() {
   const data = await db.book.findMany({
+    where: {
+      status: "ACTIVE",
+    },
     orderBy: {
       createdAt: "desc",
     },
