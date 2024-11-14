c72114db79473f4e534a86ecf3aa7631c0f28845
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-11-14 17:08:47 +0500

    feat: change personal page url in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="border-t pt-4 text-center">
       әзірлеген{" "}
-      <Link href="https://dastanozgeldi.me" className="font-bold underline">
+      <Link href="https://ozgeldi.tech" className="font-bold underline">
         @dastanozgeldi
       </Link>
     </footer>
