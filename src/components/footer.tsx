1f9373e6bd5c4ec1fa3455446bfbc4fe09cfc8aa
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-06-10 21:15:13 +0500

    feat: make footer less cringe

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export function Footer() {
   return (
     <footer className="border-t pt-4 text-center">
-      әзірлеген{" "}
-      <Link href="https://ozgeldi.tech" className="font-bold underline">
+      made by{" "}
+      <Link href="https://ozgeldi.tech" className="font-semibold underline">
         @dastanozgeldi
       </Link>
     </footer>
