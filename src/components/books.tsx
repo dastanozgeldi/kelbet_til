8f3c85d6820852d4d3c8961f9918ccc4a79dcd6b
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-07 01:31:53 +0500

    feat: display books in shadcn cards

--- src/components/books.tsx
+++ src/components/books.tsx
@@ -9,6 +9,15 @@ import {
   TermFilter,
 } from "./filters";
 import { Icons } from "./icons";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+} from "./ui/card";
+import { Button } from "./ui/button";
+import { ArrowRightIcon } from "lucide-react";
 
 export const Books = ({ data }: { data: Book[] }) => {
   const {
@@ -62,17 +71,26 @@ export const Books = ({ data }: { data: Book[] }) => {
         <h2 className="my-2 text-2xl font-bold">{term}-тоқсан</h2>
         <hr className="bg-primary h-[6px] max-w-[36px] border-0" />
       </div>
-      <div className="my-6 grid grid-cols-1 gap-4 md:grid-cols-2">
+      <div className="my-6 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {filteredBooks.map(
           (book) =>
             book.term === term && (
-              <Link
-                key={book.id}
-                href={`/${book.id}`}
-                className="min-w-[300px] rounded bg-gray-50 p-8"
-              >
-                <h2 className="text-2xl font-bold">{book.title}</h2>
-              </Link>
+              <Card key={book.id}>
+                <CardHeader>
+                  <CardTitle>{book.title}</CardTitle>
+                  <CardDescription>
+                    жүктелу күні: {book.createdAt.toLocaleDateString()}
+                  </CardDescription>
+                </CardHeader>
+                <CardFooter>
+                  <Button variant="outline" asChild>
+                    <Link href={`/${book.id}`}>
+                      Оқу
+                      <ArrowRightIcon className="size-4" />
+                    </Link>
+                  </Button>
+                </CardFooter>
+              </Card>
             ),
         )}
       </div>
