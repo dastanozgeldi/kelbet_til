1e73feb1c1b7fdf60240b7a5c3029bd79dd8016b
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-07 01:54:46 +0500

    fix: pdf reader

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,15 +1,10 @@
 "use client";
+
 import { use } from "react";
-import { pdfjs } from "react-pdf";
 
 import { Skeleton } from "@/components/ui/skeleton";
-import { PDFBook } from "./_components/pdf-book";
 import { useBook } from "./_hooks/use-book";
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.js",
-  import.meta.url,
-).toString();
+import { PDFBook } from "./_components/pdf-book";
 
 export default function Page(props: { params: Promise<{ id: string }> }) {
   const params = use(props.params);
@@ -20,7 +15,7 @@ export default function Page(props: { params: Promise<{ id: string }> }) {
       <div className="my-6 overflow-hidden">
         <div className="mb-6">
           <Skeleton className="my-2 h-10 w-full" />
-          <hr className="h-[6px] max-w-[36px] border-0 bg-primary" />
+          <hr className="bg-primary h-[6px] max-w-[36px] border-0" />
         </div>
         <Skeleton className="h-[600px]" />
       </div>
@@ -31,7 +26,7 @@ export default function Page(props: { params: Promise<{ id: string }> }) {
       <div className="my-6 overflow-hidden">
         <div className="mb-6">
           <h1 className="my-2 text-3xl font-bold md:text-4xl">{book.title}</h1>
-          <hr className="h-[6px] max-w-[36px] border-0 bg-primary" />
+          <hr className="bg-primary h-[6px] max-w-[36px] border-0" />
         </div>
         <PDFBook
           book={book}
