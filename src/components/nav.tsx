cee268db774be2d24831ecade8bd1446505ff194
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-07 02:02:51 +0500

    refactor: logo

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,5 +1,6 @@
+import Link from "next/link";
+import Image from "next/image";
 import { auth, signIn } from "@/server/auth";
-import { Logo } from "./logo";
 import { UserButton } from "./user-button";
 
 export const Nav = async () => {
@@ -7,7 +8,10 @@ export const Nav = async () => {
 
   return (
     <nav className="flex w-full items-center justify-between">
-      <Logo />
+      <Link href="/" className="flex items-center space-x-2.5">
+        <Image src="/logo.png" width={32} height={32} alt="Logo" />
+        <div className="text-xl font-bold">kelbet-til.kz</div>
+      </Link>
 
       {!session?.user ? (
         <form
