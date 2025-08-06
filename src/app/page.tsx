59b834a0dc2b9806e78233972d11e863fedaa621
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-07 04:43:03 +0500

    feat: layout visual improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,28 @@
 import Link from "next/link";
 import Image from "next/image";
-import { ArrowRightIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   return (
-    <section className="flex items-center">
-      <div className="flex w-full flex-col items-center lg:flex-row lg:justify-between lg:gap-8">
-        <div className="space-y-8 lg:w-1/2">
-          <h1 className="text-3xl font-extrabold lg:text-4xl">
-            Республика мектептерінің әдеби шығармалары бір жерде
-          </h1>
+    <div className="flex w-full flex-col items-center gap-3 lg:flex-row lg:justify-between">
+      <div className="lg:w-1/2">
+        <h1 className="my-3 text-3xl font-extrabold lg:mb-6 lg:text-4xl">
+          Республика мектептерінің әдеби шығармалары бір жерде
+        </h1>
 
-          <Button size="lg" asChild>
-            <Link href="/books">
-              Бастау
-              <ArrowRightIcon className="size-4" />
-            </Link>
-          </Button>
-        </div>
-        <div className="lg:w-1/2">
-          <Image
-            className="w-full"
-            src="/hero.png"
-            alt="hero"
-            width={500}
-            height={500}
-          />
-        </div>
+        <Button asChild className="h-12 w-60 px-6 py-2 text-lg font-medium">
+          <Link href="/books">Бастау</Link>
+        </Button>
       </div>
-    </section>
+      <div className="lg:w-1/2">
+        <Image
+          className="w-full"
+          src="/hero.png"
+          alt="hero"
+          width={500}
+          height={500}
+        />
+      </div>
+    </div>
   );
 }
