d1a6f6fef2e91905de761d2909d2775f50f0a598
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-07 04:18:55 +0500

    feat: make page header smaller

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,7 +1,7 @@
 export function PageHeader({ title }: { title: string }) {
   return (
     <div className="mb-6">
-      <h1 className="my-2 text-3xl font-bold md:text-4xl">{title}</h1>
+      <h2 className="my-2 text-2xl font-bold sm:text-3xl">{title}</h2>
       <hr className="bg-primary h-[6px] max-w-[36px] border-0" />
     </div>
   );
