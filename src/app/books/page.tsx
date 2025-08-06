879b2943de62cef5b806b109d2de824210d11172
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-07 02:40:51 +0500

    feat: implement filters in url search params

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,9 +1,24 @@
-import { Books } from "@/components/books";
+import type { Program, Language } from "@prisma/client";
 import { db } from "@/server/db";
+import Filters from "./_components/filters";
+import BookCard from "./_components/book-card";
 
-export default async function BooksPage() {
-  const data = await db.book.findMany({
+export default async function BooksPage(props: {
+  searchParams?: Promise<{
+    program?: string;
+    grade?: string;
+    language?: string;
+    term?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+
+  const books = await db.book.findMany({
     where: {
+      program: searchParams?.program as Program | undefined,
+      grade: searchParams?.grade,
+      language: searchParams?.language as Language | undefined,
+      term: searchParams?.term,
       status: "ACTIVE",
     },
     orderBy: {
@@ -11,5 +26,20 @@ export default async function BooksPage() {
     },
   });
 
-  return <Books data={data} />;
+  return (
+    <>
+      <div className="mb-6">
+        <h1 className="my-2 text-3xl font-bold md:text-4xl">Шығармалар</h1>
+        <hr className="bg-primary h-[6px] max-w-[36px] border-0" />
+      </div>
+
+      <Filters />
+
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {books.map((book) => (
+          <BookCard key={book.id} book={book} />
+        ))}
+      </div>
+    </>
+  );
 }
