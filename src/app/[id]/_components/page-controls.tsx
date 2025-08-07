f5a8677517f33af667a7d5e542ab3c9c83fd49d8
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-07 17:17:46 +0500

    feat: improve pdf book ux

--- src/app/[id]/_components/page-controls.tsx
+++ src/app/[id]/_components/page-controls.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ChevronLeftIcon, ChevronRightIcon, PencilIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
--- src/app/[id]/_components/pdf-book.tsx
+++ src/app/[id]/_components/pdf-book.tsx
@@ -2,15 +2,15 @@
 
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import type { Book } from "@prisma/client";
-import Image from "next/image";
 import { Document, Page, pdfjs } from "react-pdf";
 import { usePDFBook } from "../_hooks/use-pdf-book";
-import { Loader2Icon, XIcon } from "lucide-react";
 import PageControls from "./page-controls";
+import { useContainerWidth } from "@/hooks/use-container-width";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 export const PDFBook = ({ book }: { book: Book }) => {
+  const width = useContainerWidth();
   const { numPages, currentPage, setCurrentPage, handleDocumentLoadSuccess } =
     usePDFBook(book.id);
 
@@ -23,32 +23,13 @@ export const PDFBook = ({ book }: { book: Book }) => {
       />
 
       <Document
-        loading={
-          <div className="flex items-center justify-center gap-2">
-            <Loader2Icon className="animate-spin" />
-            Кітап ашылуда...
-          </div>
-        }
-        error={
-          <div className="flex flex-col items-center">
-            <div className="flex items-center gap-2">
-              <XIcon />
-              <span>Шығарма жүктелмеген.</span>
-            </div>
-            <Image
-              className="mt-6"
-              alt="Failed to load."
-              src="/failed.svg"
-              width={599}
-              height={417}
-            />
-          </div>
-        }
+        className="mt-3"
         file={book.fileUrl}
         onLoadSuccess={handleDocumentLoadSuccess}
         onLoadError={console.error}
       >
         <Page
+          width={width}
           noData={`Бұндай бет (${currentPage}-бет) жоқ`}
           error="Бетті жүктеуде қате туындады"
           loading="Бет жүктелуде..."
