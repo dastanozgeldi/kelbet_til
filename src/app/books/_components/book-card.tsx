a1e21171e80249eb14fb77c26c9ba6f73bf9f1b9
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-07 05:13:45 +0500

    feat: display badges in book card

--- src/app/books/_components/book-card.tsx
+++ src/app/books/_components/book-card.tsx
@@ -9,14 +9,16 @@ import {
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ArrowRightIcon } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 export default function BookCard({ book }: { book: Book }) {
   return (
     <Card key={book.id}>
       <CardHeader>
         <CardTitle>{book.title}</CardTitle>
-        <CardDescription>
-          жүктелу күні: {book.createdAt.toLocaleDateString()}
+        <CardDescription className="flex flex-wrap gap-1">
+          <Badge>{book.grade}-сынып</Badge>
+          <Badge>{book.term}-тоқсан</Badge>
         </CardDescription>
       </CardHeader>
       <CardFooter className="mt-auto">
