a52c410a42b3ed24028c94ad8a5d5a7d8f31c09b
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-10-02 19:04:58 +0900

    feat: separate main business logic from admin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
-import Link from "next/link";
-import Image from "next/image";
 import { Toaster } from "@/components/ui/sonner";
 import { EdgeStoreProvider } from "@/lib/edgestore";
-import SignInButton from "@/components/sign-in-button";
-import { UserButton } from "@/components/user-button";
-import { auth } from "@/server/auth";
 import { env } from "@/data/env/server";
 import { GoogleAnalytics } from "./google-analytics";
 import "./globals.css";
@@ -24,39 +19,13 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await auth();
 
   return (
     <html lang="en">
       <GoogleAnalytics GA_TRACKING_ID={env.GA_TRACKING_ID} />
       <body className="antialiased">
         <EdgeStoreProvider>
-          <main className="container mx-auto flex min-h-screen flex-col space-y-3 px-6">
-            <nav className="flex w-full items-center justify-between border-b py-3">
-              <Link href="/" className="flex items-center space-x-2.5">
-                <Image src="/logo.png" width={32} height={32} alt="Logo" />
-                <div className="text-xl font-bold">kelbet-til.kz</div>
-              </Link>
-
-              {!session?.user ? (
-                <SignInButton />
-              ) : (
-                <UserButton user={session.user} />
-              )}
-            </nav>
-            <div className="flex-1">{children}</div>
-            <footer className="border-t py-3 text-center">
-              made with 🥰 by{" "}
-              <a
-                href="https://instagram.com/dastanozgeldi"
-                className="font-semibold"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                @dastanozgeldi
-              </a>
-            </footer>
-          </main>
+          {children}
         </EdgeStoreProvider>
         <Toaster />
       </body>
