845ad2b00630f6a84a01f03fdddc84b94c7a580a
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-10-18 17:34:24 +0900

    feat: rewrite admin books page

--- src/app/admin/books/_components/book-actions.tsx
+++ src/app/admin/books/_components/book-actions.tsx
@@ -47,7 +47,7 @@ export const BookActions = ({ book }: { book: Book }) => {
       <DropdownMenuTrigger asChild>
         <Button
           variant="ghost"
-          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+          className="data-[state=open]:bg-muted flex h-8 w-8 p-0"
         >
           <MoreHorizontal className="h-4 w-4" />
           <span className="sr-only">Open menu</span>
