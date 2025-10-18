c632e046c53cc298cb9c912bd956a9a594d80765
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-10-18 19:37:55 +0900

    feat: implement edit book form using server actions

--- src/app/admin/books/_components/book-actions.tsx
+++ src/app/admin/books/_components/book-actions.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Language, type Book } from "@prisma/client";
+import { type Book } from "@prisma/client";
 import Link from "next/link";
 import {
   AlertDialog,
@@ -22,7 +22,6 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-import { Label } from "@/components/ui/label";
 import {
   Dialog,
   DialogContent,
@@ -30,13 +29,10 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
 import { useDeleteBook } from "../hooks/use-delete-book";
-import { useEditBook } from "../hooks/use-edit-book";
-import { filters } from "@/data/config";
+import { EditBookForm } from "./edit-book-form";
 
 export const BookActions = ({ book }: { book: Book }) => {
-  const { data, setData, handleEdit } = useEditBook(book);
   const { handleDelete } = useDeleteBook(book);
 
   const handleDownload = async () => {
@@ -88,70 +84,7 @@ export const BookActions = ({ book }: { book: Book }) => {
               <DialogHeader>
                 <DialogTitle>Шығарманы өзгерту</DialogTitle>
               </DialogHeader>
-              <form onSubmit={handleEdit} className="space-y-4">
-                <div className="space-y-1.5">
-                  <Label htmlFor="title">Кітап атауы</Label>
-                  <Input
-                    id="title"
-                    type="text"
-                    value={data.title || ""}
-                    placeholder="Абай жолы"
-                    onChange={(e) =>
-                      setData({ ...data, title: e.target.value })
-                    }
-                  />
-                </div>
-                <div className="space-y-1.5">
-                  <Label htmlFor="grade">Сынып</Label>
-                  <select
-                    id="grade"
-                    className="w-full rounded-md border bg-transparent px-3 py-2"
-                    defaultValue={data.grade || filters.grades[0]}
-                    onChange={(e) =>
-                      setData({ ...data, grade: e.target.value })
-                    }
-                  >
-                    {filters.grades.map((grade) => (
-                      <option key={grade} value={grade}>
-                        {grade}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div className="space-y-1.5">
-                  <Label htmlFor="language">Тіл</Label>
-                  <select
-                    id="language"
-                    className="w-full rounded-md border bg-transparent px-3 py-2"
-                    defaultValue={data.language || filters.languages[0]}
-                    onChange={(e) =>
-                      setData({ ...data, language: e.target.value as Language })
-                    }
-                  >
-                    {filters.languages.map((language) => (
-                      <option key={language} value={language}>
-                        {language}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div className="space-y-1.5">
-                  <Label htmlFor="term">Тоқсан</Label>
-                  <select
-                    id="term"
-                    className="w-full rounded-md border bg-transparent px-3 py-2"
-                    defaultValue={data.term || filters.terms[0]}
-                    onChange={(e) => setData({ ...data, term: e.target.value })}
-                  >
-                    {filters.terms.map((term) => (
-                      <option key={term} value={term}>
-                        {term}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <Button>Сақтау</Button>
-              </form>
+              <EditBookForm book={book} />
             </DialogContent>
           </Dialog>
           <DropdownMenuItem onSelect={handleDownload}>Жүктеу</DropdownMenuItem>
