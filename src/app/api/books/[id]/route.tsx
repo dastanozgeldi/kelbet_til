eb5a7e6322172ca0567a14fe24b41aeda7a9ce05
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-10-18 19:40:36 +0900

    refactor: get rid of unused routes and hooks

--- src/app/api/books/[id]/route.tsx
+++ src/app/api/books/[id]/route.tsx
@@ -1,28 +1,4 @@
-import { type Language } from "@prisma/client";
 import { db } from "@/server/db";
-
-export async function PATCH(
-  request: Request,
-  props: { params: Promise<{ id: string }> },
-) {
-  const params = await props.params;
-  const { data } = await request.json();
-
-  const book = await db.book.update({
-    where: {
-      id: params.id,
-    },
-    data: {
-      title: data.title,
-      fileUrl: data.fileUrl,
-      grade: data.grade,
-      language: data.language as Language,
-      term: data.term,
-    },
-  });
-
-  return Response.json({ book });
-}
 
 export async function DELETE(
   request: Request,
