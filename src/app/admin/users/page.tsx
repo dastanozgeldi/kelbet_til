58fceda9ec154b7ef9b5014a48010b46a016cc46
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-10-18 16:47:09 +0900

    feat: debloat admin users table

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,22 +1,62 @@
 import { Metadata } from "next";
 import { db } from "@/server/db";
-import { auth } from "@/server/auth";
-import { columns } from "./columns";
-import { UsersTable } from "./users-table";
-import { User } from "next-auth";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Suspense } from "react";
+import { Badge } from "@/components/ui/badge";
 
 export const metadata: Metadata = {
   title: "Қолданушылар",
 };
 
 export default async function Page() {
-  const session = await auth();
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SuspenseBoundary />
+    </Suspense>
+  );
+}
 
+async function SuspenseBoundary() {
   const users = await db.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+    },
     orderBy: { role: "desc" },
+    take: 10,
   });
 
-  const data = users.filter((user: User) => user.id !== session?.user.id);
-
-  return <UsersTable columns={columns} data={data} />;
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Аты-жөні</TableHead>
+          <TableHead>Поштасы</TableHead>
+          <TableHead>Рөл</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {users.map((user) => (
+          <TableRow key={user.id}>
+            <TableCell>{user.name}</TableCell>
+            <TableCell>{user.email}</TableCell>
+            <TableCell>
+              <Badge variant="outline">
+                {user.role === "ADMIN" ? "Админ" : "Қолданушы"}
+              </Badge>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
