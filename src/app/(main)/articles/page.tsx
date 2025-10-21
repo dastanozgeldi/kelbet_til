8cef9ab8bf64bae8ba2df7d1410bd3e3bec500cf
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-10-21 19:43:26 +0900

    feat: init basic rich text editor

--- src/app/(main)/articles/page.tsx
+++ src/app/(main)/articles/page.tsx
@@ -1,14 +1,22 @@
-import { Metadata } from "next";
+"use client";
+
 import { PageHeader } from "../_components/page-header";
+import RichTextEditor from "@/components/rich-text-editor";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
-export const metadata: Metadata = {
-  title: "Мақалалар",
-};
+export default function Page() {
+  const [post, setPost] = useState("");
 
-export default async function Page() {
   return (
     <>
       <PageHeader title="Мақалалар" />
+
+      <div className="mx-auto h-full w-full max-w-3xl py-8 flex flex-col gap-3">
+        <RichTextEditor content={post} onChange={setPost} />
+
+        <Button className="self-end w-32" onClick={() => console.log(post)}>Save</Button>
+      </div>
     </>
   );
 }
---
+++ src/components/rich-text-editor/index.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import React from "react";
+import MenuBar from "./menu-bar";
+
+interface RichTextEditorProps {
+  content: string;
+  onChange: (content: string) => void;
+}
+export default function RichTextEditor({
+  content,
+  onChange,
+}: RichTextEditorProps) {
+  const editor = useEditor({
+    immediatelyRender: false,
+    extensions: [
+      StarterKit.configure({
+        bulletList: {
+          HTMLAttributes: {
+            class: "list-disc ml-3",
+          },
+        },
+        orderedList: {
+          HTMLAttributes: {
+            class: "list-decimal ml-3",
+          },
+        },
+      }),
+    ],
+    content: content,
+    editorProps: {
+      attributes: {
+        class: "min-h-[156px] border rounded-md bg-slate-50 py-2 px-3",
+      },
+    },
+    onUpdate: ({ editor }) => {
+      onChange(editor.getHTML());
+    },
+  });
+
+  return (
+    <div>
+      <MenuBar editor={editor} />
+      <EditorContent editor={editor} />
+    </div>
+  );
+}
