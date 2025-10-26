53abac5b5455b3ba58476a4e330ba0465b62af93
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-10-26 14:06:11 +0900

    feat: admin create article

--- src/app/(main)/articles/page.tsx
+++ src/app/(main)/articles/page.tsx
@@ -1,6 +1,17 @@
 import { Metadata } from "next";
+import Link from "next/link";
+import { ArrowRightIcon } from "lucide-react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card";
 import { PageHeader } from "../_components/page-header";
-import Editor from "./_components/editor";
+import { Button } from "@/components/ui/button";
+import { db } from "@/server/db";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Мақалалар",
@@ -11,7 +22,36 @@ export default async function Page() {
     <>
       <PageHeader title="Мақалалар" />
 
-      <Editor />
+      <Suspense fallback={<>loading articles...</>}>
+        <ArticlesGrid />
+      </Suspense>
     </>
+  );
+}
+
+async function ArticlesGrid() {
+  const articles = await db.article.findMany({ take: 9 });
+
+  return (
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      {articles.map((article) => (
+        <Card key={article.id}>
+          <CardHeader>
+            <CardTitle>{article.title}</CardTitle>
+            <CardDescription className="flex flex-wrap gap-1">
+              {article.description}
+            </CardDescription>
+          </CardHeader>
+          <CardFooter className="mt-auto">
+            <Button variant="outline" asChild>
+              <Link href={`/articles/${article.slug}`}>
+                Оқу
+                <ArrowRightIcon className="size-4" />
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
   );
 }
